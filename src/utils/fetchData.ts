9bedc278da861bbee83b60c6ad2cec0f28478af7
Maksim Kanev <maks_kv@yahoo.com>
2025-06-23 17:35:12 +0300

    refactored the fetchData to catch erros

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,7 +1,11 @@
-import axios from "axios";
-import { CoinData } from "@/types";
+import { CoinData } from '@/types';
 
 export async function fetchCryptoData(): Promise<CoinData[]> {
-  const res = await axios.get("/api/prices");
-  return res.data;
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error('Failed to fetch crypto data');
+  }
+  const data: CoinData[] = await response.json();
+  return data;
 }
