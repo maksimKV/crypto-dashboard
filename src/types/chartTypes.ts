0b87da4ee72cf8cfb7d6f068ff7a8e24c04cccbd
Maksim Kanev <maks_kv@yahoo.com>
2025-06-23 18:42:50 +0300

    added new interface for the store and merged the interfaces into a single file

--- src/types/chartTypes.ts
+++ src/types/chartTypes.ts
@@ -1,3 +1,27 @@
 export interface CryptoChartProps {
-    coinId?: string;
-  }
+  coinId: string;
+  data?: MarketChartData;
+}
+
+export interface MarketChartData {
+  prices: [number, number][];
+  market_caps: [number, number][];
+  total_volumes: [number, number][];
+}
+
+export interface CoinData {
+  id: string;
+  symbol: string;
+  name: string;
+  image: string;
+  current_price: number;
+  market_cap: number;
+  market_cap_rank: number;
+  total_volume: number;
+  high_24h: number;
+  low_24h: number;
+  price_change_24h: number;
+  price_change_percentage_24h: number;
+  circulating_supply: number;
+  total_supply: number | null;
+}
