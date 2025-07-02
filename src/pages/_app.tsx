e39af309408e79c32ec343342cd57063fb8221eb
Maksim Kanev <maks_kv@yahoo.com>
2025-07-02 16:01:17 +0300

    Wraped entire app in ErrorBoundary for global error handling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import '@/styles/globals.css'
 import { Provider } from 'react-redux';
 import { store } from '@/store';
 import type { AppProps } from 'next/app';
+import { ErrorBoundary } from '@/components/ErrorBoundary';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <ErrorBoundary>
+        <Component {...pageProps} />
+      </ErrorBoundary>
     </Provider>
   );
 }
